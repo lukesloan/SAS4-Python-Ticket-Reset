235f5bbdb6508ea0bfe7210e2f786980f278edd6
lukesloan <166965898+lukesloan@users.noreply.github.com>
2024-06-18 18:05:06 -0500

    Delete ticket_reset_cmd.bat
    useless now that main python file no longer requires admin privileges

--- ticket_reset_cmd.bat
+++
@@ -1,6 +0,0 @@
-::Gives python elevated privileges to change date
-
-::Change to directory where ticket_reset.py is located
-cd C:\Users\llslo\OneDrive\Desktop\Misc\games\Misc files\SAS\python\python ticket reset\v1_4
-
-python ticket_reset_v1_4.py
