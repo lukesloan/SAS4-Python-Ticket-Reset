5c77080e93f22379cdd041d96e989be775165b3b
lukesloan <166965898+lukesloan@users.noreply.github.com>
2024-06-18 17:55:51 -0500

    Add files via upload

--- ticket_reset_v1_4.py
+++ ticket_reset_v1_4.py
@@ -15,7 +15,7 @@ def wait_for_key():
     print("Press any key to continue...")
     msvcrt.getch()  # Waits for a key press
 
-# Opens GUI window where user navigates to a requested directory to save for future reference in ticket_reset_filepaths.csv
+# Opens GUI window where user navigates to a requested directory to save for future reference n ticket_reset_filepaths.csv
 def get_directory_path(row):
     root = tk.Tk()
     root.withdraw()  # Hide the root window
@@ -27,7 +27,8 @@ def get_directory_path(row):
         directory_path = filedialog.askdirectory(title="Select current version directory")
     elif row == 2:
         directory_path = filedialog.askdirectory(title="Select Steam game directory")
-
+    elif row == 3:
+        directory_path = filedialog.askdirectory(title="Select RunAsDate directory")
     if directory_path:
         return directory_path
     else:
@@ -49,6 +50,7 @@ def check_setup():
                     ['"1.10.2 location"',''],  
                     ['"Current game version location"',''], 
                     ['"Steam game directory location"',''], 
+                    ['"RunAsDate.exe location"',''], 
                     ]
 
                 with open(file_path, 'w', newline='') as csvfile:
@@ -94,11 +96,13 @@ def read_filepaths_csv():
             directory_1_10_2 = rows[0][1] if rows[0][0] == '"1.10.2 location"' else None
             directory_current = rows[1][1] if rows[1][0] == '"Current game version location"' else None
             directory_game = rows[2][1] if rows[2][0] == '"Steam game directory location"' else None
+            directory_RunAsDate = rows[3][1] if rows[3][0] == '"RunAsDate.exe location"' else None
             print("1.10.2 location: ", directory_1_10_2)
             print("Current game version location: ", directory_current)
             print("Steam game directory location: ", directory_game)
+            print("RunAsDate.exe location location: ", directory_game)
 
-            return directory_1_10_2, directory_current, directory_game
+            return directory_1_10_2, directory_current, directory_game, directory_RunAsDate
         
     except FileNotFoundError:
         print("File " + file_path + " not found.")
@@ -147,7 +151,7 @@ def copy_files(source_dir, destination_dir, program_name):
     try:
         if not source_dir.strip() or not destination_dir.strip():
             # print("At least one of the filepaths is undefined. Run the setup script")
-            raise Exception("At least one of the filepaths is undefined. Run the setup script")
+            raise Exception("At least one of the filepaths is undefined. Delete ticket_reset_filepaths.csv if it exists and run the script again")
         
         source_dir_1 = source_dir + r'\Assets\Loc'
         destination_dir_1 = destination_dir + r'\Assets\Loc'
@@ -193,21 +197,24 @@ def start_program(directory_game, program_name):
         print(msg3)
 
 # Takes current date, increments date 1 day forward, and sets that as the new date. Also returns original date
-def change_system_date_forward():
+def change_system_date_forward(program_name, directory_RunAsDate):
+    if not directory_RunAsDate.strip():
+        # print("At least one of the filepaths is undefined. Run the setup script")
+        raise Exception("Game directory is undefined. Run the setup script")
+    RunAsDate_path = directory_RunAsDate + '\\' + 'RunAsDate.exe'
     try:
         # Get the current date
         current_date = datetime.date.today()
-        formatted_current_date = current_date.strftime("%m-%d-%y")
 
         # Increment the date by one day
         next_date = current_date + datetime.timedelta(days=1)
         formatted_next_date = next_date.strftime("%m-%d-%y")
 
         # Set the system date using Command Prompt (requires administrative privileges)
-        subprocess.run(['date', str(formatted_next_date)], shell=True, check=True)
+        subprocess.run(RunAsDate_path + ' Days:+1 Attach:' + program_name, shell=True, check=True)
         msg1 = "System date changed forward by one day to: " + str(formatted_next_date)
         print(msg1)
-        return formatted_current_date  # Return the original current date
+        
     except subprocess.CalledProcessError as e:
         msg2 = "Error: " + str(e)
         print(msg2)
@@ -230,10 +237,10 @@ if __name__ == "__main__":
     program_name = "SAS4-Win.exe" 
     
     # Sees if ticket_reset_filepaths.csv exists, and if not, makes it
-    check_setup() # works
+    check_setup() 
 
     # Reads above csv file for necessary ticket reset directories
-    directory_1_10_2, directory_current, directory_game = read_filepaths_csv() # works
+    directory_1_10_2, directory_current, directory_game, directory_RunAsDate = read_filepaths_csv() 
 
     # Closes SAS if you already have SAS open, will continue on if SAS is closed
     close_process(program_name) # works
@@ -250,7 +257,7 @@ if __name__ == "__main__":
     wait_for_key() # works
 
     # Changes date to tomorrow and also returns current date as original_date
-    original_date = change_system_date_forward() # works
+    change_system_date_forward(program_name, directory_RunAsDate)
     
     # Have to be force backup-ed or gracefully close out of SAS in order for save to update, waits for user to do so manually
     # todo: automate the navigation
@@ -262,9 +269,6 @@ if __name__ == "__main__":
 
     # Copies version 2.0.1 files and pastes them in game directory
     copy_files(directory_current, directory_game, program_name) # works
-
-    # Returns date back to original_date
-    change_system_date_back(original_date) # works
 
     # Opens SAS from game directory (version 2.0.1)
     start_program(directory_game, program_name) # works
