14fa778e38cd467f088e9a974e701dba7feeb2cb
lukesloan <166965898+lukesloan@users.noreply.github.com>
2024-06-19 13:24:40 -0500

    Add files via upload
    formatting changes

--- ticket_reset_cmd.bat
+++ ticket_reset_cmd.bat
@@ -1,8 +1,8 @@
-::Optional batch script to make it more convenient to run python ticket_reset_v1_5.py
-::Just double-click this script to run it, no more run as administrator required
-::Alternatively, use VS Code/however else you run python scripts
+:: Optional batch script to make it more convenient to run python ticket_reset.py
+:: Just double-click this script to run it, no more run as administrator required
+:: Alternatively, use VS Code/however else you run python scripts
 
-::Change to directory where ticket_reset.py is located
+:: Change to directory where ticket_reset.py is located
 cd C:\add\the\directory\here\in\this\format
 
 python ticket_reset.py
